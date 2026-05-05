a28c7fe68ecc498bcae403ab81b05638e5eda317
sarratwa <sarra2969@gmail.com>
2026-05-05 12:00:00 +0200

    installations

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import cv2
 import tensorflow as tf
 
 print(tf.config.list_physical_devices('GPU'))
-# print(tf.__version__)
-
-# print("TensorFlow:", tf.__version__)
+print(tf.__version__)
+print("TensorFlow:", tf.__version__)
 # print("GPUs:", tf.config.list_physical_devices("GPU"))
 
 # tensorflow only works on Linux -> switch to wsl
